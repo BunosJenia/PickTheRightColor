c6ec0d1c4facf666caa5570c4de73d7695593a5f
Evgeny Bunos <evgeny.bunos@wrenkitchens.com>
2020-07-07 15:45:50 +0300

    The Game implementation

--- PickTheRightColor/GameScene.swift
+++ PickTheRightColor/GameScene.swift
@@ -9,81 +9,282 @@
 import SpriteKit
 import GameplayKit
 
+var square0: SKSpriteNode?
+var square1: SKSpriteNode?
+var square2: SKSpriteNode?
+var square3: SKSpriteNode?
+
+let squareSize = CGSize(width: 250, height: 250)
+
+var mainLabel: SKLabelNode?
+var scoreLabel: SKLabelNode?
+var timerLabel: SKLabelNode?
+
+let squarePositionOffset = CGFloat(150)
+let labelOffsetY = CGFloat(400)
+let offBlackColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+let offWhiteColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0)
+
+let color0 = UIColor.orange
+let color1 = UIColor.green
+let color2 = UIColor.blue
+let color3 = UIColor.purple
+
+let incorrectColor0 = UIColor.yellow
+let incorrectColor1 = UIColor.red
+let incorrectColor2 = UIColor.lightGray
+
+let colorArrayString = ["Orange", "Green", "Blue", "Purple"]
+var colorArrayChoice = 0
+var correctSquare = 0
+var colorChoice = 0
+
+var touchLocation: CGPoint?
+var touchedNodes: [SKNode?] = []
+
+var score = 0
+var isAlive = true
+var countDownVar = 13
+
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     override func didMove(to view: SKView) {
+        self.backgroundColor = offBlackColor
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        spawnMainLabel()
+        spawnTimerLabel()
+        spawnScoreLabel()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        spawnSquare0()
+        spawnSquare1()
+        spawnSquare2()
+        spawnSquare3()
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        randomizeColors()
+        countDownTimer()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for t in touches {
+            touchLocation = t.location(in: self)
+            touchedNodes = nodes(at: touchLocation!)
+            
+            touchedNodeLogic()
+        }
+    }
+    
+    func touchedNodeLogic() {
+        if touchedNodes.isEmpty == false {
+            if touchedNodes[0]?.name != "correct" {
+                gameOverLogic()
+                isAlive = false
+            } else if touchedNodes[0]?.name == "correct" {
+                addToScore()
+                randomizeColors()
+            }
+        }
+    }
+    
+    func spawnSquare0() {
+        square0 = SKSpriteNode(color: offWhiteColor, size: squareSize)
+        square0?.position = CGPoint(x: -squarePositionOffset, y: squarePositionOffset)
+        
+        self.addChild(square0!)
+    }
+    
+    func spawnSquare1() {
+        square1 = SKSpriteNode(color: offWhiteColor, size: squareSize)
+        square1?.position = CGPoint(x: squarePositionOffset, y: squarePositionOffset)
+        
+        self.addChild(square1!)
+    }
+    
+    func spawnSquare2() {
+        square2 = SKSpriteNode(color: offWhiteColor, size: squareSize)
+        square2?.position = CGPoint(x: -squarePositionOffset, y: -squarePositionOffset)
+        
+        self.addChild(square2!)
+    }
+    
+    func spawnSquare3() {
+        square3 = SKSpriteNode(color: offWhiteColor, size: squareSize)
+        square3?.position = CGPoint(x: squarePositionOffset, y: -squarePositionOffset)
+        
+        self.addChild(square3!)
+    }
+    
+    func spawnMainLabel() {
+        mainLabel = SKLabelNode(fontNamed: "Futura")
+        
+        mainLabel?.fontSize = 80
+        mainLabel?.fontColor = offWhiteColor
+        mainLabel?.position = CGPoint(x: 0, y: labelOffsetY)
+        mainLabel?.text = "Start!"
+        
+        self.addChild(mainLabel!)
+    }
+    
+    func spawnScoreLabel() {
+        scoreLabel = SKLabelNode(fontNamed: "Futura")
+        
+        scoreLabel?.fontSize = 40
+        scoreLabel?.fontColor = offWhiteColor
+        scoreLabel?.position = CGPoint(x: 0, y: -labelOffsetY - 80)
+        scoreLabel?.text = "Score: \(score)"
+        
+        self.addChild(scoreLabel!)
+    }
+    
+    func spawnTimerLabel() {
+        timerLabel = SKLabelNode(fontNamed: "Futura")
+        
+        timerLabel?.fontSize = 100
+        timerLabel?.fontColor = offWhiteColor
+        timerLabel?.position = CGPoint(x: 0, y: -labelOffsetY)
+        timerLabel?.text = "10"
+        
+        self.addChild(timerLabel!)
+    }
+    
+    func randomizeColors() {
+        colorArrayChoice = Int.random(in: 0..<4)
+        colorChoice = Int.random(in: 0..<4)
+        correctSquare = Int.random(in: 0..<4)
+        
+        printColors()
+        printColorCorrectSquare()
+    }
+    
+    func printColors() {
+        if colorChoice == 0 {
+            mainLabel?.fontColor = color0
+        } else if colorChoice == 1{
+            mainLabel?.fontColor = color1
+        } else if colorChoice == 2 {
+            mainLabel?.fontColor = color2
+        } else if colorChoice == 3 {
+            mainLabel?.fontColor = color3
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        mainLabel?.text = "\(colorArrayString[colorArrayChoice])"
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func printColorCorrectSquare() {
+        let tempColor = [color0, color1, color2, color3]
+        
+        if colorChoice == 0 {
+            square0?.color = tempColor[colorChoice]
+            square1?.color = incorrectColor0
+            square2?.color = incorrectColor1
+            square3?.color = incorrectColor2
+            
+            square0?.name = "correct"
+            square1?.name = "incorrect0"
+            square2?.name = "incorrect1"
+            square3?.name = "incorrect2"
+        }
+        
+        if colorChoice == 1 {
+            square0?.color = incorrectColor2
+            square1?.color = tempColor[colorChoice]
+            square2?.color = incorrectColor0
+            square3?.color = incorrectColor1
+            
+            square0?.name = "incorrect0"
+            square1?.name = "correct"
+            square2?.name = "incorrect1"
+            square3?.name = "incorrect2"
+        }
+        
+        if colorChoice == 2 {
+            square0?.color = incorrectColor1
+            square1?.color = incorrectColor2
+            square2?.color = tempColor[colorChoice]
+            square3?.color = incorrectColor0
+            
+            square0?.name = "incorrect0"
+            square1?.name = "incorrect1"
+            square2?.name = "correct"
+            square3?.name = "incorrect2"
+        }
+        
+        if colorChoice == 3 {
+            square0?.color = incorrectColor2
+            square1?.color = incorrectColor1
+            square2?.color = incorrectColor0
+            square3?.color = tempColor[colorChoice]
+            
+            square0?.name = "incorrect0"
+            square1?.name = "incorrect1"
+            square2?.name = "incorrect2"
+            square3?.name = "correct"
+        }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func addToScore() {
+        score += 1
+        scoreLabel?.text = "Score: \(score)"
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func gameOverLogic() {
+        mainLabel?.fontColor = offWhiteColor
+        mainLabel?.text = "Game Over"
+
+        timerLabel?.text = "Try Again!"
+
+        square0?.removeFromParent()
+        square1?.removeFromParent()
+        square2?.removeFromParent()
+        square3?.removeFromParent()
+        
+        resetTheGame()
     }
     
+    func resetTheGame() {
+        let wait = SKAction.wait(forDuration: 3)
+        
+        let theGameScene = GameScene(size: self.size)
+        let theTransition = SKTransition.crossFade(withDuration: 0.5)
+
+        theGameScene.scaleMode = SKSceneScaleMode.aspectFill
+
+        let sceneChange = SKAction.run {
+           self.scene?.view?.presentScene(theGameScene, transition : theTransition)
+        }
+
+        let sequence = SKAction.sequence([wait, sceneChange])
+        self.run(SKAction.repeat(sequence, count: 1))
+    }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func countDownTimer() {
+        let wait = SKAction.wait(forDuration: 1.0)
+        
+        let countDown = SKAction.run {
+            if isAlive == true {
+                countDownVar -= 1
+            }
+            
+            if countDownVar <= 10 && isAlive == true {
+                timerLabel?.text = "\(countDownVar)"
+            }
+            
+            if countDownVar <= 0 {
+                self.gameOverLogic()
+            }
+        }
+        
+        let sequence = SKAction.sequence([wait, countDown])
+        self.run(SKAction.repeat(sequence, count: countDownVar))
+    }
+    
+    func resetGameVariablesOnStart() {
+        spawnSquare0()
+        spawnSquare1()
+        spawnSquare2()
+        spawnSquare3()
+        
+        score = 0
+        countDownVar = 13
+        isAlive = true
     }
 }
